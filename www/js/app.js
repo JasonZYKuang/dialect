4932b9a9b4d41eb206feead89a44860cdd69be51
JasonZYKuang <kzy00007@163.com>
2016-07-01 18:13:26 +0800

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -15,6 +15,24 @@ angular.module('starter', ['ionic','ngCordova', 'starter.controllers','starter.n
   })
     .run(function ($ionicPlatform,$rootScope,$window,$document,DeviceStatus,TranslateService) {
       TranslateService.load('YANGJIANG');
+      localforage.getItem('lang', function(err, value){
+          if (err){
+          } else if (value == null){
+            localforage.setItem('lang', {id:lang.id,name:lang.name});
+          } else {
+        	lang = value;
+        	$rootScope.lang = lang;
+          }
+        });
+      localforage.getItem('yuyin_lang', function(err, value){
+          if (err){
+          } else if (value == null){
+            localforage.setItem('yuyin_lang', {id:Luyin.id,name:Luyin.name});
+          } else {
+            Luyin = value;
+            $rootScope.Luyin = Luyin;
+          }
+      });
 
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
